e15ffb95c26916b307662845b143a54c43e22844
Ueber <uebersyemmer@gmail.com>
2021-06-20 01:38:32 -0300

    Adicionando mais uma linha.

--- es3_lanchonete/src/br/com/es3_lanchonete/git/Main.java
+++ es3_lanchonete/src/br/com/es3_lanchonete/git/Main.java
@@ -16,10 +16,8 @@ public class Main {
 		// TODO Auto-generated method stub
 		//sysout ctrl + espaço
 		System.out.println("Bem-vindo à Lanchonete da Engenharia de Software 3!");
-		System.out.println("Add uma nova linha");
-		System.out.println("Add outra nova linha");
-		System.out.println("Add terceira nova linha");
-		System.out.println("Add quarta nova linha");
+		System.out.println("Adicionando outra linha");
+
 
 	}
 

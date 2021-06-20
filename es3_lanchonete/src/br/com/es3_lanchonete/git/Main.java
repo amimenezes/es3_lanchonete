d5ae422fba16b407ba23c433ff68176d04abec56
mml88 <mi.menezes.luiz@gmail.com>
2021-06-19 23:35:36 -0300

    Arquivos Iniciais add via Upload

---
+++ es3_lanchonete/src/br/com/es3_lanchonete/git/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package br.com.es3_lanchonete.git;
+
+/**
+ * @author Chele
+ * Integração do Eclipse com o GitHub
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//sysout ctrl + espaço
+		System.out.println("Bem-vindo à Lanchonete da Engenharia de Software 3!");
+
+	}
+
+}
